9ae6960d11d9260cd2cd94606e7f4044a37d13ba
Damien Walsh <me@damow.net>
2017-09-28 23:57:20 +0100

    Cleanup: Code cleanup

--- main.c
+++ main.c
@@ -9,35 +9,12 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 #include <limits.h>
-#include "gpio.h"
+#include "vospi.h"
 
 static const char *device = "/dev/spidev0.0";
-static uint8_t mode = SPI_MODE_3 | SPI_NO_CS;
+static uint8_t mode = SPI_MODE_3;
 static uint8_t bits = 8;
 static uint32_t speed = 18000000;
-
-// The chip-select pin
-#define CS_BCM_PIN 25
-
-// The size of a single VOSPI packet
-#define VOSPI_PACKET_BYTES 164
-
-// The number of packets per segment
-#define VOSPI_PACKETS_PER_SEGMENT 60
-
-// The number of segments per frame
-#define VOSPI_SEGMENTS_PER_FRAME 4
-
-// An array containing the bytes from a single packet
-uint8_t lepton_packet[VOSPI_PACKET_BYTES];
-
-typedef struct vospi_packet {
-	uint8_t symbols[VOSPI_PACKET_BYTES];
-} vospi_packet_t;
-
-typedef struct vospi_segment {
-	vospi_packet_t packets[VOSPI_PACKETS_PER_SEGMENT];
-} vospi_segment_t;
 
 /**
  * Transfer a single VoSPI segment.
@@ -45,8 +22,7 @@ typedef struct vospi_segment {
 void transfer_segment(int fd, vospi_segment_t* segment)
 {
 	// Perform the spidev transfer
-	int ret = read(fd, &segment->packets[0].symbols, VOSPI_PACKET_BYTES);
-	if (ret < 1) {
+	if (read(fd, &segment->packets[0].symbols, VOSPI_PACKET_BYTES) < 1) {
 		perror("SPI: failed to transfer packet");
 		exit(-5);
 	}
@@ -57,8 +33,7 @@ void transfer_segment(int fd, vospi_segment_t* segment)
 	}
 
 	// Read the remaining packets
-	ret = read(fd, &segment->packets[1].symbols, VOSPI_PACKET_BYTES * (VOSPI_PACKETS_PER_SEGMENT - 1));
-	if (ret < 1) {
+	if (read(fd, &segment->packets[1].symbols, VOSPI_PACKET_BYTES * (VOSPI_PACKETS_PER_SEGMENT - 1)) < 1) {
 		perror("SPI: failed to transfer the rest of the segment");
 		exit(-5);
 	}
@@ -68,13 +43,6 @@ int main(int argc, char *argv[])
 {
 	int ret = 0;
 	int fd;
-
-	// Set up the GPIO-based CS pin
-	printf("Setting up GPIO-CS...\n");
-	gpio_export(CS_BCM_PIN);
-	gpio_direction(CS_BCM_PIN, GPIO_OUT);
-	gpio_write(CS_BCM_PIN, GPIO_HIGH);
-
 
 	// Open the spidev device
 	printf("Opening device...\n");
@@ -111,10 +79,6 @@ int main(int argc, char *argv[])
 		segments[seg] = malloc(sizeof(vospi_segment_t));
 	}
 
-	// Select the chip
-	gpio_write(CS_BCM_PIN, GPIO_LOW);
-	usleep(100000);
-
 	// Keep streaming segments until we receive a valid, first segment to sync
 	printf("Synchronising with first segment...\n");
 	uint8_t ttt_bits, packet_20_num;
@@ -130,16 +94,14 @@ int main(int argc, char *argv[])
 			if (packet_20_num != 20) {
 					// Deselect the chip, wait 200ms with CS deasserted
 					printf("Waiting to reset...\n");
-					gpio_write(CS_BCM_PIN, GPIO_HIGH);
 					usleep(185000);
-					gpio_write(CS_BCM_PIN, GPIO_LOW);
 					continue;
 			}
 
 			// Check we're looking at the first segment, if not, just keep reading until we get there
 			ttt_bits = segments[0]->packets[20].symbols[0] >> 4;
 			printf("TTT Bits: %d\t P20 Num: %d\n", ttt_bits, packet_20_num);
-			if (ttt_bits == 1 && packet_20_num == 20) {
+			if (ttt_bits == 1) {
 				break;
 			}
 	}
